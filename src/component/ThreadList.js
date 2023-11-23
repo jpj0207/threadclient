32189ebd20811c868f827ef8b1e16c026c08b6c9
jpj0207 <jpj0207@gmail.com>
2023-11-23 11:09:47 +0900

    commit4

--- src/component/ThreadList.js
+++ src/component/ThreadList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 import Header from './Header';
@@ -8,19 +8,19 @@ const ThreadList = () => {
   const { board } = useParams();
   const server_URL = `https://threadserver.netlify.app/${board}`;
 
-  // fetchData 함수를 외부로 빼서 재사용
-  const fetchData = async () => {
+  // useCallback을 사용하여 fetchData 감싸기
+  const fetchData = useCallback(async () => {
     try {
       const res = await axios.get(server_URL);
       setThreads(res.data);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
-  };
+  }, [server_URL]); // 의존성 배열에 server_URL 추가
 
   useEffect(() => {
     fetchData();
-  }, [fetchData]);
+  }, [fetchData]); // 의존성 배열에 fetchData 추가
 
   return (
     <div>
@@ -44,8 +44,3 @@ const ThreadList = () => {
 };
 
 export default ThreadList;
-
-//url파싱 :board값 받기, board값에 맞는 threadlist 백엔드에 요청, threadlist 정렬
-//thread값으로 detail로 사용자정의 css전달 
-//board이름 헤더 밑에 보여주고 링크 걸어서 threadlist(선택한board)로 오게하기=>Thread.js,postThread.js에도 적용
-//언급은 >>1234 언급된거는 thread옆에 ex)강현씨 떨여졌나요?<<1234 / >>1234 칠팔이횽이떨어진건당연...[더보기]
